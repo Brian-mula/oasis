b78f5056aae1ad7d91067212fbe98efe4ddb1710
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-21 19:26:02 +0300

    add image

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,34 @@
-import supabase from "./supabase";
+
+import supabase, { supabaseUrl } from "./supabase";
+
+export async function createCabin (cabin){
+    const imageName = `${Math.random()}-${cabin.image.name}`.replaceAll("/","");
+    const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`
+
+const { data, error } = await supabase
+.from('cabins')
+.insert([
+  {...cabin, image: imagePath},
+])
+.select()
+if(error) {
+    console.log(error)
+    throw new Error(error.message)
+}
+const { data:uploadData, error:storageError } = await supabase.storage.from('cabin-images').upload(imageName, cabin.image)
+  if (storageError) {
+    await supabase
+.from('cabins')
+.delete()
+.eq('id', data.id)
+    throw new Error(storageError.message)
+  } 
+  else{
+    console.log(uploadData)
+  }
+return data;
+
+}
 
 export async function getCabins (){
 
