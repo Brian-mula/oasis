0a536aa6e0613f996e9269421175d887a7d576a2
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-21 14:13:58 +0300

    modified API cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,3 +11,16 @@ if(error) {
 }
 return data;
 }
+
+export async function deleteCabin(id){
+
+const { error } = await supabase
+.from('cabins')
+.delete()
+.eq('id', id)
+if(error) {
+    console.log(error)
+    throw new Error(error.message)
+}
+
+}
