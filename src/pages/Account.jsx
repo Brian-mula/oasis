cace48a83420d6e18e6b8a74b075c2f665560e36
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-21 11:41:13 +0300

    modified accounts

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,20 +1,8 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
 
 function Account() {
   return (
     <>
-      <Heading as="h1">Update your account</Heading>
-
-      <Row>
-        <Heading as="h3">Update user data</Heading>
-        <p>Update user data form</p>
-      </Row>
-
-      <Row>
-        <Heading as="h3">Update password</Heading>
-        <p>Update user password form</p>
-      </Row>
+     <h1>Accounts</h1>
     </>
   );
 }
