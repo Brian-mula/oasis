2f020737d0de150c0b8975dbbfb7715bffc1ebba
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-21 14:13:47 +0300

    modified cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,20 @@
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
   
   return (
     
     <div>
-      <div className="flex justify-between w-full items-start">
+      <div className="flex justify-between w-full items-start mb-3">
       <h1 className="text-xl font-semibold">Cabins</h1>
 
+      <div className="flex items-center">
       <p>Filter</p>
+      <p>Sort</p>
+      <CreateCabinForm/>
+
+      </div>
       </div>
       <CabinTable />
     </div>
