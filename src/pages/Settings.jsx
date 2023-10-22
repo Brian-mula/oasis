06c8b7e77bb69f47ec3faa945b722f101dc9b0ed
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-22 19:48:58 +0300

    modified settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,8 +1,10 @@
+import UpdateSettingsForm from "../features/settings/UpdateSettingsForm";
 
 function Settings() {
   return (
     <div>
-      Settings
+      <h1 className="font-bold text-xl my-3">Update Hotel Settings</h1>
+      <UpdateSettingsForm/>
     </div>
   )
 }
