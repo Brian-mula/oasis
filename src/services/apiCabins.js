a934e1290fef629ac4702c93c15555873aa85ddb
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-22 18:34:09 +0300

    done with cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -40,6 +40,20 @@ if(error) {
 return data;
 }
 
+export async function getCabin (id){
+  const { data: cabin, error:cabinError } = await supabase
+  .from('cabins')
+  .select("*")
+  .eq('id', id)
+  .single();
+  if(cabinError) {
+      console.log(cabinError)
+      throw new Error(cabinError.message)
+  }
+
+  return cabin;
+}
+
 export async function deleteCabin(id){
 
 const { error } = await supabase
@@ -50,5 +64,21 @@ if(error) {
     console.log(error)
     throw new Error(error.message)
 }
+
+}
+
+export async function updateCabin (cabin, id){
+  console.log(cabin)
+
+const { data, error } = await supabase
+.from('cabins')
+.update(cabin)
+.eq('id', id)
+.select()
+if(error) {
+    console.log(error)
+    throw new Error(error.message)
+}
+console.log(data)
 
 }
