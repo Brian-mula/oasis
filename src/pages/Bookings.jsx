47d03ab5a9b0889bbfa8ffa14661007590e1448a
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-23 11:13:49 +0300

    modified booking

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,8 +1,15 @@
+import BookingTable from "../features/bookings/BookingTable";
+import BackButton from "../ui/BackButton";
+
 
 function Bookings() {
   return (
    <div>
-    <h1>Bookings</h1>
+   <div className="flex justify-between items-start mt-4">
+    <BackButton/>
+    <h1 className="text-xl font-semibold">Bookings</h1>
+   </div>
+   <BookingTable/>
    </div>
   );
 }
