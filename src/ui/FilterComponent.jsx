dfb817630218393a6e42ca152885288be9c6205b
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-23 11:45:24 +0300

    made filter component resusable

--- src/ui/FilterComponent.jsx
+++ src/ui/FilterComponent.jsx
@@ -1,17 +1,20 @@
 import { useSearchParams } from "react-router-dom"
 
 
-export default function FilterComponent() {
+export default function FilterComponent({param,options}) {
     const [searchParam, setSearchParam] = useSearchParams()
     const handleFilter = (value) => {
-        searchParam.set('discount', value)
+        searchParam.set(param, value)
         setSearchParam(searchParam)
     }
   return (
     <div className='flex p-2 shadow-sm rounded-sm'>
-        <button onClick={()=>handleFilter('all')} className='btn hover:bg-indigo-800 mx-1 hover:text-white'>All</button>
-        <button onClick={()=>handleFilter('with-discount')} className='btn hover:bg-indigo-800 mx-1 hover:text-white'>With Discount</button>
-        <button onClick={()=>handleFilter('no-discount')} className='btn hover:bg-indigo-800 mx-1 hover:text-white'>No discount</button>
+       {
+            options.map((option) => (
+                <button onClick={()=>handleFilter(option.value)} className='btn hover:bg-indigo-800 mx-1 hover:text-white' key={option.value} >{option.label}</button>
+            ))
+       }
+        
     </div>
   )
 }
