9406d32495d1ab7fc86815e588b3eac98994016f
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-23 11:44:07 +0300

    added cabin item

--- src/features/cabins/CabinItem.jsx
+++ src/features/cabins/CabinItem.jsx
@@ -1,14 +1,25 @@
+import { FiEye, FiTrash } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { formatCurrency } from "../../utils/helpers";
+import { useDeleteSelectedCabin } from "./useDeleteCabin";
 
-export default function CabinItem({cabin: {name, maxCapacity, regularPrice, discount, image}}) {
-    
+export default function CabinItem({cabin}) {
+  const {isDeleting,deleteCabin}= useDeleteSelectedCabin();
   return (
-    <div className="gird grid-cols-6 gap-2">
-        <img src={image} alt="" className="h-12 w-12 rounded-sm"/>
-        <div>{name}</div>
-        <div>{maxCapacity}</div>
-        <div>{regularPrice}</div>
-        <div>{discount}</div>
-        <div></div>
-    </div>
+    <>
+         <img src={cabin.image} alt="" className="h-20 w-20 rounded-sm"/>
+         <div>{cabin.name}</div>
+         <div>Fits {cabin.maxCapacity} guests</div>
+         <div>{formatCurrency(cabin.regularPrice)}</div>
+         <div>{formatCurrency(cabin.discount)}</div>
+         <div className="flex justify-center items-start">
+          <button disabled={isDeleting} onClick={()=>deleteCabin(cabin.id)} className="btn btn-sm btn-square">
+            <FiTrash/>
+          </button>
+          <Link to={`${cabin.id}`} className="btn btn-sm btn-success btn-square mx-1">
+            <FiEye/>
+          </Link>
+         </div>
+         </>
   )
 }
