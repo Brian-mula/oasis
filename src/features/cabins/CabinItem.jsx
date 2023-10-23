c7e08b23d0922c8732ec927b4743453ef8c177a2
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-23 18:49:37 +0300

    modified cabin item

--- src/features/cabins/CabinItem.jsx
+++ src/features/cabins/CabinItem.jsx
@@ -1,25 +1,22 @@
-import { FiEye, FiTrash } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import TableOperation from "../../ui/TableOperation";
 import { formatCurrency } from "../../utils/helpers";
 import { useDeleteSelectedCabin } from "./useDeleteCabin";
 
-export default function CabinItem({cabin}) {
-  const {isDeleting,deleteCabin}= useDeleteSelectedCabin();
+export default function CabinItem({ cabin }) {
+  const { isDeleting, deleteCabin } = useDeleteSelectedCabin();
   return (
     <>
-         <img src={cabin.image} alt="" className="h-20 w-20 rounded-sm"/>
-         <div>{cabin.name}</div>
-         <div>Fits {cabin.maxCapacity} guests</div>
-         <div>{formatCurrency(cabin.regularPrice)}</div>
-         <div>{formatCurrency(cabin.discount)}</div>
-         <div className="flex justify-center items-start">
-          <button disabled={isDeleting} onClick={()=>deleteCabin(cabin.id)} className="btn btn-sm btn-square">
-            <FiTrash/>
-          </button>
-          <Link to={`${cabin.id}`} className="btn btn-sm btn-success btn-square mx-1">
-            <FiEye/>
-          </Link>
-         </div>
-         </>
-  )
+      <img src={cabin.image} alt="" className="h-20 w-20 rounded-sm" />
+      <div>{cabin.name}</div>
+      <div>Fits {cabin.maxCapacity} guests</div>
+      <div>{formatCurrency(cabin.regularPrice)}</div>
+      <div>{formatCurrency(cabin.discount)}</div>
+
+      <TableOperation
+        path={`${cabin.id}`}
+        onClick={() => deleteCabin(cabin.id)}
+        condition={isDeleting}
+      />
+    </>
+  );
 }
