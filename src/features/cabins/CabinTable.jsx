6f67395d442c13ceab842e63239901c81cb72823
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-23 11:44:19 +0300

    modified cabin item

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,19 +1,17 @@
 // import styled from "styled-components";
 
-import { FiEye, FiTrash } from "react-icons/fi";
 
 
-import { Link, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Spinner from "../../ui/Spinner";
-import { formatCurrency } from "../../utils/helpers";
+import CabinItem from "./CabinItem";
 import { useCabins } from "./useCabin";
-import { useDeleteSelectedCabin } from "./useDeleteCabin";
 
 //const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4ZGNwZm9jdm91bmp1cHR4ZXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc3ODcwNzIsImV4cCI6MjAxMzM2MzA3Mn0.nraYMfkXnWBgf9r9mkrUos3ZSe_vCkp9hdrM-_1IwCQ"
 
 export default function CabinTable() {
  
-  const {isDeleting,deleteCabin}= useDeleteSelectedCabin();
+  
   const {
     cabins,
     isLoading,
@@ -49,21 +47,7 @@ export default function CabinTable() {
       </div>
       <div className="grid grid-cols-6 gap-2">
       {filteredCabins.map((cabin) => (
-         <>
-         <img src={cabin.image} alt="" className="h-20 w-20 rounded-sm"/>
-         <div>{cabin.name}</div>
-         <div>Fits {cabin.maxCapacity} guests</div>
-         <div>{formatCurrency(cabin.regularPrice)}</div>
-         <div>{formatCurrency(cabin.discount)}</div>
-         <div className="flex justify-center items-center">
-          <button disabled={isDeleting} onClick={()=>deleteCabin(cabin.id)} className="btn btn-sm btn-square">
-            <FiTrash/>
-          </button>
-          <Link to={`${cabin.id}`} className="btn btn-sm btn-success btn-square mx-1">
-            <FiEye/>
-          </Link>
-         </div>
-         </>
+         <CabinItem key={cabin.id} cabin={cabin}/>
          
          ))}
          </div>
