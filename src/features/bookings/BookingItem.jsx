c7cfda3dc5bb1e8609d68bf3c0bab34503444996
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-23 18:49:16 +0300

    modified booking items

--- src/features/bookings/BookingItem.jsx
+++ src/features/bookings/BookingItem.jsx
@@ -1,10 +1,11 @@
+import TableOperation from "../../ui/TableOperation";
 import { formatCurrency, formatDate } from "../../utils/helpers";
 
 
 export default function BookingItem({booking}) {
   return (
     <>
-        <div className="w-60">
+        <div className="w-52">
             <p className="truncate w-full">Name: {booking.guests.fullName}</p>
             <p className="truncate w-full">Email: {booking.guests.email}</p>
         </div>
@@ -13,8 +14,9 @@ export default function BookingItem({booking}) {
            <p>Start: {formatDate(booking.startDate)}</p>
            <p>End: {formatDate(booking.endDate)}</p>
           </div>
-          <div > <p className={`bg-blue-100 rounded-full px-2 py-1 text-blue-700 ${booking.status === 'checked-in' ? 'bg-green-600 text-white':''} `}>{booking.status}</p></div>
+          <div > <p className={`bg-blue-100 rounded-full px-2 py-1 text-blue-700 ${booking.status === 'checked-in' ? 'bg-green-600 text-white':''} `}>{booking.status.replace("-"," ")}</p></div>
           <div>{formatCurrency(booking.totalPrice)}</div>
+          <TableOperation path={`${booking.id}`} />
     </>
   )
 }
