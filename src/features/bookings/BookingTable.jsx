7c5c5ca240e71601d2a659fe1ed8ee15fa44918c
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-23 11:13:08 +0300

    modified booking table

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,31 +1,43 @@
-import BookingRow from "./BookingRow";
-import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
+import { useQuery } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+import Spinner from "../../ui/Spinner";
+import BookingItem from "./BookingItem";
 
-function BookingTable() {
-  const bookings = [];
+
+export default function BookingTable() {
+ const {data:bookings=[],isloading,error} = useQuery({
+  queryKey:['bookings'],
+  queryFn: getBookings
+ })
+
+ if(isloading) {
+  return <Spinner/>;
+ }
+  
+  
+  if(error) {
+    return <p>error</p>;
+  }
+  
+  console.log(bookings);
 
   return (
-    <Menus>
-      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
-        <Table.Header>
-          <div>Cabin</div>
-          <div>Guest</div>
-          <div>Dates</div>
-          <div>Status</div>
-          <div>Amount</div>
-          <div></div>
-        </Table.Header>
-
-        <Table.Body
-          data={bookings}
-          render={(booking) => (
-            <BookingRow key={booking.id} booking={booking} />
-          )}
-        />
-      </Table>
-    </Menus>
+    <>
+    <div className="flex  justify-center p-3 shadow-md rounded-sm mt-4">
+     <div>
+     <div className="grid grid-cols-4 gap-x-10 gap-y-5 shadow-md mb-2">
+          <div className="font-bold text-lg">Guest</div>
+          <div className="font-bold text-lg">Dates</div>
+          <div className="font-bold text-lg">Status</div>
+          <div className="font-bold text-lg">Amount</div>
+      </div>
+      <div className="grid grid-cols-4 gap-x-10 gap-y-5">
+          {bookings.map((booking) => (<BookingItem key={booking.id} booking={booking}/>))}
+      </div>
+     </div>
+    </div>
+    </>
   );
 }
 
-export default BookingTable;
+
