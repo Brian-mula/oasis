252629b2bd8d8857cb34e3834253d273e3ae7bf0
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-25 13:45:12 +0300

    added api auth

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,36 +1,52 @@
 import supabase from "./supabase";
 
-export async function login({email,password}){
-
-const { data, error } = await supabase.auth.signInWithPassword({
+export async function login({ email, password }) {
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
-    password
-  })
-  if(error){
-    console.log(error)
-    throw new Error(error.message)
+    password,
+  });
+  if (error) {
+    console.log(error);
+    throw new Error(error.message);
   }
-  console.log(data)
+  console.log(data);
 
   return data;
-  
 }
 
-export async function getCurrentUser(){
-    const{data:session}= await supabase.auth.getSession()
-    if(!session.session){
-        return null;
-    }
-    const {data,error}=await supabase.auth.getUser()
-    if(error){
-        throw new Error(error.message)
-    }
-    return data?.user;
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) {
+    return null;
+  }
+  const { data, error } = await supabase.auth.getUser();
+  if (error) {
+    throw new Error(error.message);
+  }
+  return data?.user;
 }
 
-export async function logout(){
-    const { error } = await supabase.auth.signOut()
-    if(error){
-        throw new Error(error.message)
-    }
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function signUp({ email, password, name }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        name,
+        avater:""
+      },
+    },
+  });
+  if (error) {
+    throw new Error(error.message);
+  }
+  console.log(data);
+  return data;
 }
