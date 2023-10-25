fc6e7ed3e434c5457ce25d8eb61c8a65eed00c8d
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-25 10:14:14 +0300

    modified api auth

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -15,3 +15,22 @@ const { data, error } = await supabase.auth.signInWithPassword({
   return data;
   
 }
+
+export async function getCurrentUser(){
+    const{data:session}= await supabase.auth.getSession()
+    if(!session.session){
+        return null;
+    }
+    const {data,error}=await supabase.auth.getUser()
+    if(error){
+        throw new Error(error.message)
+    }
+    return data?.user;
+}
+
+export async function logout(){
+    const { error } = await supabase.auth.signOut()
+    if(error){
+        throw new Error(error.message)
+    }
+}
