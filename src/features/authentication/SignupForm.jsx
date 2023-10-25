6496e65c4c0c4fae5d812722950d40a8072d07ec
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-25 13:44:28 +0300

    added sign up form

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,38 +1,15 @@
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
 
-// Email regex: /\S+@\S+\.\S+/
+import BackButton from '../../ui/BackButton'
+import CreateUserForm from './CreateUserForm'
 
-function SignupForm() {
+export default function SignupForm() {
   return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
-      </FormRow>
+    <div>
+      <div className='flex justify-between items-start my-3'>
+        <BackButton/>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
-      </FormRow>
-
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
-      </FormRow>
-
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
-      </FormRow>
-
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button>Create new user</Button>
-      </FormRow>
-    </Form>
-  );
+        <CreateUserForm/>
+      </div>
+    </div>
+  )
 }
-
-export default SignupForm;
