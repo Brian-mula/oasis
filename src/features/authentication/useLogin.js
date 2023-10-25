94b78e5320cc242c1b91a26dbec5589f950adbb7
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-25 10:13:37 +0300

    modified use login

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,22 +1,27 @@
 
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuth";
 
 export function useLogin() {
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
     
     const{mutate:login,isLoading:isLogingIn}=useMutation({
+        
         mutationFn: ({email,password})=> loginApi({email,password}),
-        onSuccess:()=>{
+        onSuccess:(user)=>{
+            queryClient.setQueryData(['user'],user.user)
+            console.log(user)
             toast.success(`Login was successful`);
-            navigate("/");
+            navigate("/dashboard");
             
         },
         onError:(error)=>{
             toast.error(`Login failed: ${error.message}`);
         }
     })
+    console.log(isLogingIn)
     return {login,isLogingIn}
 }
