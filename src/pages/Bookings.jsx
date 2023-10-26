afd070bc373d1fdfe37c73b9346b91103d0dace4
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-26 20:54:03 +0300

    modified bookings

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,3 +1,4 @@
+import { HiPlus } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 import BookingTable from "../features/bookings/BookingTable";
 import BackButton from "../ui/BackButton";
@@ -26,10 +27,11 @@ function Bookings() {
     <div>
       <div className="flex justify-between items-start mt-4">
         <BackButton />
-        <div className="flex">
+        <div className="flex items-center">
           <FilterComponent options={options} param="status" />
-          <Link to="/new-booking" className="mx-2 p-1 ">
-            New Booking
+          <Link to="/new-booking" className="mx-2 p-1 btn btn-success flex text-white ">
+            <HiPlus className="text-2xl font-bold text-white"/>
+            <span>New Booking</span>
           </Link>
         </div>
       </div>
