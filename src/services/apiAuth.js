8d667752ada120b004da44dede062ea457029080
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-26 10:01:16 +0300

    api auth

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
@@ -49,4 +49,40 @@ export async function signUp({ email, password, name }) {
   }
   console.log(data);
   return data;
+}
+
+export async function updateUser({name,avater,password}){
+    let updateData;
+    if(password){
+        updateData={password}
+    }
+    if(name){
+        updateData={data:{name}}
+    }
+const { data, error } = await supabase.auth.updateUser(updateData);
+if(error){
+    throw new Error(error.message)
+}
+if(!avater){
+    return data;
+}
+const fileName = `avater-${data.user.id}-${Math.random()}`;
+
+const{error:storageError} = await supabase.storage.from('avaters').upload(fileName,avater);
+if(storageError){
+    throw new Error(storageError.message)
+}
+
+const{data:updateduser, error:avaterError} = await supabase.auth.updateUser({
+    data:{
+        avater:`${supabaseUrl}/storage/v1/object/public/avaters/${fileName}`
+    }
+})
+if(avaterError){
+    throw new Error(avaterError.message)
+}
+return updateduser;
+
+
+  
 }
