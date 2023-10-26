10d3737366bc9c3749ebee3d08fc906555414ed2
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-26 12:50:53 +0300

    modified booking

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,35 +1,40 @@
+import { Link } from "react-router-dom";
 import BookingTable from "../features/bookings/BookingTable";
 import BackButton from "../ui/BackButton";
 import FilterComponent from "../ui/FilterComponent";
 
-
 function Bookings() {
   const options = [
     {
-      label:'All',
-      value:'all'
+      label: "All",
+      value: "all",
     },
     {
-      label:'Unconfirmed',
-      value:'unconfirmed'
+      label: "Unconfirmed",
+      value: "unconfirmed",
     },
     {
-      label:'Checked In',
-      value:'checked-in'
+      label: "Checked In",
+      value: "checked-in",
     },
     {
-      label:'Checked Out',
-      value:'checked-out'
-    }
-  ]
+      label: "Checked Out",
+      value: "checked-out",
+    },
+  ];
   return (
-   <div>
-   <div className="flex justify-between items-start mt-4">
-    <BackButton/>
-    <FilterComponent options={options} param="status"/>
-   </div>
-   <BookingTable/>
-   </div>
+    <div>
+      <div className="flex justify-between items-start mt-4">
+        <BackButton />
+        <div className="flex">
+          <FilterComponent options={options} param="status" />
+          <Link to="/new-booking" className="mx-2 p-1 ">
+            New Booking
+          </Link>
+        </div>
+      </div>
+      <BookingTable />
+    </div>
   );
 }
 
