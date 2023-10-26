62fb15a83f451e6fb2e895425d489f37c73862d6
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-26 20:52:50 +0300

    added use delete

--- src/features/bookings/useDeleteBooking.js
+++ src/features/bookings/useDeleteBooking.js
@@ -1,14 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
 
 export function useDeleteBooking(){
+    const navigate = useNavigate();
     const queryClient = useQueryClient();
     const {mutate:deleteBooking,isLoading:isDeleting} = useMutation({
         mutationFn:(id)=>deleteBookingApi(id),
         onSuccess:()=>{
             toast.success(`Booking  was deleted successfully`);
             queryClient.invalidateQueries({active:true});
+            navigate("/bookings",{replace:true});
         },
         onError:(error)=>{
             toast.error(`Delete booking failed: ${error.message}`);
