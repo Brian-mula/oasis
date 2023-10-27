3cbbf7d4f58145a158461c9aa6297c457025424c
Brian-mula <mulatiwangilabrian@gmail.com>
2023-10-27 12:33:29 +0300

    added loader

---
+++ src/ui/Loaded.jsx
@@ -0,0 +1,13 @@
+
+import { MoonLoader } from 'react-spinners'
+
+export default function Loaded() {
+  return (
+    <div className="flex flex-col justify-center h-[calc(100vh-7rem)] w-[calc(100vw-17rem)]">
+    <div className=" ml-auto mr-auto">
+      <MoonLoader color="#36d7b7" />
+      
+    </div>
+  </div>
+  )
+}
